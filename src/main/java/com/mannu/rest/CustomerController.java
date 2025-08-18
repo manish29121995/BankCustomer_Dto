c863757f4001c327bc8c2107bf8b2fc52817ec5a
manish <manishprajapati2915@gmail.com>
2025-08-18 12:07:49 +0530

    update and patch method implementation

--- src/main/java/com/mannu/rest/CustomerController.java
+++ src/main/java/com/mannu/rest/CustomerController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -95,7 +98,7 @@ public class CustomerController {
 				
 	}
 	
-	@GetMapping("/delete/{accNum}")
+	@DeleteMapping("/delete/{accNum}")
 	public ResponseEntity<?> deleteCustomer(@PathVariable Long accNum) {
 		try {
 		String deleteAccount = service.deleteAccount(accNum);
@@ -104,6 +107,33 @@ public class CustomerController {
 			return ResponseEntity
 					.badRequest()
 					.body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/update/{accNum}")
+	public ResponseEntity<?> updateCUstomer(@PathVariable Long accNum,@RequestBody Customer_Dto dto){
+		try {
+			String updateCustomer = service.updateCustomer(accNum, dto);
+			return new ResponseEntity<String>(updateCustomer,HttpStatus.OK);
+		}catch (IllegalArgumentException e) {
+        
+			return ResponseEntity
+					.badRequest()
+					.body(e.getMessage());
+		}
+	}
+	
+	@PatchMapping("/patch/{accNum}")
+	public ResponseEntity<?> patchUpdate(@PathVariable Long accNum,	@RequestBody Customer_Dto dto){
+		
+		try {
+   String patchUpdate = service.patchUpdate(accNum, dto);
+	return new ResponseEntity<String>(patchUpdate,HttpStatus.OK);		
+		}catch (IllegalArgumentException e) {
+			
+   return ResponseEntity
+		.badRequest()
+		.body(e.getMessage());
 		}
 	}
 }
